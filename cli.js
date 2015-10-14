d0d9b93676a36dd63d82a0bc4fad654b2610a372
Kiko Beats <josefrancisco.verdu@gmail.com>
2015-10-14 22:05:18 +0200

    const not supported in strict mode

--- cli.js
+++ cli.js
@@ -1,25 +1,26 @@
 #!/usr/bin/env node
 /* eslint-disable no-mixed-spaces-and-tabs */
 'use strict';
-const updateNotifier = require('update-notifier');
-const meow = require('meow');
-const trash = require('trash');
+var updateNotifier = require('update-notifier');
+var meow = require('meow');
+var trash = require('trash');
 
-const cli = meow(`
-	Usage
-	  $ trash <path> [...]
-
-	Example
-	  $ trash unicorn.png rainbow.png
-`, {
-	string: ['_']
+var cli = meow([
+  'Usage',
+  '  $ trash <path> [...]',
+  'Example',
+  '  $ trash unicorn.png rainbow.png'
+].join('\n'), {
+  string: ['_']
 });
 
-updateNotifier({pkg: cli.pkg}).notify();
+updateNotifier({
+  pkg: cli.pkg
+}).notify();
 
 if (cli.input.length === 0) {
-	console.error('Specify at least one path');
-	process.exit(1);
+  console.error('Specify at least one path');
+  process.exit(1);
 }
 
 trash(cli.input);
